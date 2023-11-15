74546cb2187c228d15e971862f01138e623f9a7e
Jordan H <jdhassy@hotmail.com>
2023-11-14 20:21:11 -0600

    Creating a componenet based design, so that we can use object oriented programming

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@
 
 import { useEffect, useState } from "react";
 import "./App.css";
+import TextBox from "./TextBox";
+
 function App() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetch(`/api/users`)
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
+
+  let description = "This is a description.";
+  let question = "Huh?";
+  let title = "Welcome to the world of Terraquor!";
+  let options = [{option: "option1", action: () => {alert('option 1 clicked')}},
+  {option: "option2", action: () => {alert('option 2 clicked')}},
+  {option: "option3", action: () => {alert('option 3 clicked')}},
+  {option: "option4", action: () => {alert('option 4 clicked')}},
+  {option: "option5", action: () => {alert('option 5 clicked')}},
+  {option: "option6", action: () => {alert('option 6 clicked')}}];
+  
   return (
-    <div className="App">
-      <h1>Users</h1>
-      {users.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
+    <div className="App" style={{backgroundImage: "url(/titleImage.jpg)"}}>
+      <TextBox options = {options} description={description} question={question} title={title}/>
     </div>
   );
 }
